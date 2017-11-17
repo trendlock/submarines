cf55f80b54377a25aa9a2874e9858289902c2338
Aidan Morrison <aidandmorrison@gmail,com>
2017-11-17 13:12:23 +1100

    first basic working one

---
+++ scripts/basic.R
@@ -0,0 +1,179 @@
+library(dev)
+library(plotly)
+
+
+######### SCENARIO 1   ##############
+
+speeds <- c(1:20)  ## knots
+hotel.1 <- c(rep(150,20))  ## kw
+eff.jet <- c(0.08,0.125,0.2, 0.3, 0.35, 0.42, 0.46, 0.5, 0.56, 0.6, 0.62, 0.58, 0.57, 0.55, 0.54, 0.54, 0.54, 0.54, 0.54, 0.53)
+#eff.jet <- c(0.05,0.07,0.1, 0.15, 0.2, 0.25, 0.28, 0.3, 0.33, 0.35, 0.37, 0.4, 0.42, 0.43, 0.45, 0.48, 0.52, 0.54, 0.55, 0.57)
+total.batt <- 500 #Tonnes, 
+batt.dens <- 0.15
+
+patrol <- 2
+power.mob.drawn.jet.patrol <- hotel.1[1]
+power.mob.req.patrol <- power.mob.drawn.jet.patrol*eff.jet[patrol]
+const <- power.mob.req.patrol/(patrol^3)
+
+df <- tibble(speeds, hotel.1, eff.jet)
+colnames(df) <- c("speed", "hotel.1", "eff.jet")
+
+df <- df %>% 
+  mutate(eff.prop = 1-log(speed+3)/6 +0.10 - speeds/500 ) %>%
+  mutate(power.mob.req = const*speed^3,
+         power.mob.drawn.jet = power.mob.req/eff.jet,
+         power.mob.drawn.prop = power.mob.req/eff.prop,
+         power.tot.jet = power.mob.drawn.jet + hotel.1,
+         power.tot.prop = power.mob.drawn.prop + hotel.1,
+         total.energy = total.batt*batt.dens*1000,  ##MJ
+         endurance.prop.hour = total.energy*1000/power.tot.prop/3600,
+         endurance.jet.hour = total.energy*1000/power.tot.jet/3600,
+         range.prop = endurance.prop.hour*speed,
+         range.jet = endurance.jet.hour*speed)
+
+df2 <- df %>% 
+  gather(key = "tail", value = "value", endurance.prop.hour, endurance.jet.hour)
+
+plot <- df %>% 
+  ggplot(aes(x = speed))+
+  geom_line(aes(y = eff.prop, col = "efficiency prop")) +
+  geom_line(aes(y = eff.jet, col = "efficiency jet"))+
+  labs(title = "Efficiency Assumption Scenario 1 (patrol 2kt, transit 8kt)")+
+  theme(axis.title.y = element_blank())
+ggplotly(plot)%>%  
+  layout(legend = list(orientation = 'v', y = 0.9, x = 0.7))
+
+  ## - speeds/120
+
+plot <- df2 %>% 
+  ggplot(aes(x = speed, y = value, col = tail))+
+  geom_line()+
+  geom_point()+
+  geom_text(aes(label = round(value, digits = 1)), nudge_y = 2, angle = 45)+
+  #geom_text(aes(label = round(value, digits = 1)), position = position_dodge(4))+
+  labs(title = "Endurance Scenario 1 (patrol 2kt, transit 8kt)")+
+  #theme(axis.title.x = element_blank(), axis.title.y = element_blank())+
+  theme(legend.title=element_blank())
+ggplotly(plot) %>%  
+  layout(legend = list(orientation = 'v',y = 0.8, x = 0.7))
+
+# plot <- df %>% 
+#   ggplot(aes(x = speed))+
+#   geom_line(aes(y = endurance.prop.hour, col = "Propeller Endurance"))+
+#   geom_text()+
+#   geom_line(aes(y = endurance.jet.hour, col = "Jet Endurance"))+
+#   labs(title = "Endurance Comparison")+
+#   #theme(axis.title.x = element_blank(), axis.title.y = element_blank())+
+#   theme(legend.title=element_blank())
+# ggplotly(plot) %>%  
+#   layout(legend = list(orientation = 'v',y = 0.8, x = 0.7))
+
+plot <- df %>% 
+  ggplot(aes(x = speeds))+
+  geom_line(aes(y = range.prop, col = "Propeller Range nm"))+
+  geom_line(aes(y = range.jet, col = "Jet Range nm"))+
+  labs(title = "Range Scenario 1 (patrol 2kt, transit 8kt)")
+ggplotly(plot) %>%  
+  layout(legend = list(orientation = 'v', y = 0.8, x = 0.6))
+
+plot <- df %>% 
+  ggplot(aes(x = speeds))+
+  geom_line(aes(y = hotel.1, col = "hotel")) +
+  geom_line(aes(y = power.mob.drawn.jet, col = "power drawn by jet"))+
+  geom_line(aes(y = power.mob.req, col = "power thrust required"))+
+  geom_line(aes(y = power.mob.drawn.prop, col = "power drawn by prop"))+
+  labs(title = "Power Scenario 1 (patrol 2kt, transit 8kt)")
+ggplotly(plot) %>%  
+  layout(legend = list(orientation = 'v', y = 0.8, x = 0.1))
+
+
+############# SCENARIO 2 #############
+
+speeds <- c(1:20)  ## knots
+hotel.1 <- c(rep(150,20))  ## kw
+#eff.jet <- c(0.08,0.125,0.2, 0.3, 0.35, 0.42, 0.46, 0.5, 0.56, 0.6, 0.62, 0.58, 0.57, 0.55, 0.54, 0.54, 0.54, 0.54, 0.54, 0.53)
+eff.jet <- c(0.05,0.07,0.1, 0.15, 0.2, 0.25, 0.28, 0.3, 0.33, 0.35, 0.37, 0.4, 0.42, 0.43, 0.45, 0.48, 0.52, 0.54, 0.55, 0.57)
+total.batt <- 500 #Tonnes, 
+batt.dens <- 0.15
+
+patrol <- 3
+power.mob.drawn.jet.patrol <- hotel.1[1]
+power.mob.req.patrol <- power.mob.drawn.jet.patrol*eff.jet[patrol]
+const <- power.mob.req.patrol/(patrol^3)
+
+df <- tibble(speeds, hotel.1, eff.jet)
+colnames(df) <- c("speed", "hotel.1", "eff.jet")
+
+df <- df %>% 
+  mutate(eff.prop = 1-log(speed+3)/6 +0.10 - speeds/200 ) %>%
+  mutate(power.mob.req = const*speed^3,
+         power.mob.drawn.jet = power.mob.req/eff.jet,
+         power.mob.drawn.prop = power.mob.req/eff.prop,
+         power.tot.jet = power.mob.drawn.jet + hotel.1,
+         power.tot.prop = power.mob.drawn.prop + hotel.1,
+         total.energy = total.batt*batt.dens*1000,  ##MJ
+         endurance.prop.hour = total.energy*1000/power.tot.prop/3600,
+         endurance.jet.hour = total.energy*1000/power.tot.jet/3600,
+         range.prop = endurance.prop.hour*speed,
+         range.jet = endurance.jet.hour*speed)
+
+df2 <- df %>% 
+  gather(key = "tail", value = "value", endurance.prop.hour, endurance.jet.hour)
+
+plot <- df %>% 
+  ggplot(aes(x = speed))+
+  geom_line(aes(y = eff.prop, col = "efficiency prop")) +
+  geom_line(aes(y = eff.jet, col = "efficiency jet"))+
+  labs(title = "Efficiency Assumption Scenario 2 (patrol 3kt, transit 12kt)")+
+  theme(axis.title.y = element_blank())
+ggplotly(plot)%>%  
+  layout(legend = list(orientation = 'v', y = 0.9, x = 0.7))
+
+## - speeds/120
+
+plot <- df2 %>% 
+  ggplot(aes(x = speed, y = value, col = tail))+
+  geom_line()+
+  geom_point()+
+  geom_text(aes(label = round(value, digits = 1)), nudge_y = 2, angle = 45)+
+  #geom_text(aes(label = round(value, digits = 1)), position = position_dodge(4))+
+  labs(title = "Endurance Scenario 2 (patrol 3kt, transit 12kt)")+
+  #theme(axis.title.x = element_blank(), axis.title.y = element_blank())+
+  theme(legend.title=element_blank())
+ggplotly(plot) %>%  
+  layout(legend = list(orientation = 'v',y = 0.8, x = 0.7))
+
+# plot <- df %>% 
+#   ggplot(aes(x = speed))+
+#   geom_line(aes(y = endurance.prop.hour, col = "Propeller Endurance"))+
+#   geom_text()+
+#   geom_line(aes(y = endurance.jet.hour, col = "Jet Endurance"))+
+#   labs(title = "Endurance Comparison")+
+#   #theme(axis.title.x = element_blank(), axis.title.y = element_blank())+
+#   theme(legend.title=element_blank())
+# ggplotly(plot) %>%  
+#   layout(legend = list(orientation = 'v',y = 0.8, x = 0.7))
+
+plot <- df %>% 
+  ggplot(aes(x = speeds))+
+  geom_line(aes(y = range.prop, col = "Propeller Range nm"))+
+  geom_line(aes(y = range.jet, col = "Jet Range nm"))+
+  labs(title = "Range Scenario 2 (patrol 3kt, transit 12kt)")
+ggplotly(plot) %>%  
+  layout(legend = list(orientation = 'v', y = 0.8, x = 0.6))
+
+plot <- df %>% 
+  ggplot(aes(x = speeds))+
+  geom_line(aes(y = hotel.1, col = "hotel")) +
+  geom_line(aes(y = power.mob.drawn.jet, col = "power drawn by jet"))+
+  geom_line(aes(y = power.mob.req, col = "power thrust required"))+
+  geom_line(aes(y = power.mob.drawn.prop, col = "power drawn by prop"))+
+  labs(title = "Power Scenario 2 (patrol 3kt, transit 12kt)")
+ggplotly(plot) %>%  
+  layout(legend = list(orientation = 'v', y = 0.8, x = 0.1))
+
+
+
+
+
