2874a8cfca37b14c824e7560fb0a518d6041cfbb
rosseji <rosseireland@gmail.com>
2017-11-23 07:21:44 +1100

    add round

---
+++ R/roundUp.R
@@ -0,0 +1,9 @@
+
+#' @export
+roundUp <- function(x) 10^ceiling(log10(x))
+
+#' @export
+roundUpNice <- function(x, nice=c(1,2,4,5,6,8,10)) {
+  if(length(x) != 1) stop("'x' must be of length 1")
+  10^floor(log10(x)) * nice[[which(x <= 10^floor(log10(x)) * nice)[[1]]]]
+}
